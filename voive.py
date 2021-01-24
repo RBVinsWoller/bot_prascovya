fa171b5e1210b93135de6a6011e8889645f3c7d9
Peter <39239992+RBVinsWoller@users.noreply.github.com>
2021-01-24 15:16:26 +0400

    voice test update

---
+++ voive.py
@@ -0,0 +1,17 @@
+#тестим воис
+import os
+import time
+import speech_recognation as sr
+import fuzzywuzzy as fuzz
+import pyttsx3
+import datetime
+
+words = {
+    'name': ('прасковья', 'кова', 'пара'),
+    'delete': ('скажи', '')
+
+
+
+
+ 
+}
